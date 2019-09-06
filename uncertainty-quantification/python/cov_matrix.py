f7b90d9216a034ddfa79fc6524a68835c04dc1fa
tobiasstenzel <tobias.stenzel@mailbox.org>
2019-09-06 13:57:00 +0200

    probably cov matrix was much simpler than thought

--- uncertainty-quantification/python/cov_matrix.py
+++ uncertainty-quantification/python/cov_matrix.py
@@ -12,54 +12,6 @@ import numpy as np
 import pandas as pd
 import respy as rp
 
-
-def var_epsilon(mean_left, mean_right, var_left, var_right):
-    """
-    Computes the Variance of the sum of the product of a number of pairs
-    of random variables. All random variables in the sum are iid.
-
-    Paramters
-    ---------
-    mean_left: array_like
-        1d-vector of means.
-    mean_right: array_like
-        1d-vector of means.
-    var_left: array_like
-        1d-vector of variances.
-    var_right: array_like
-        1d-vector of variances.
-
-    Returns
-    -------
-    var: int
-        Variance of the sum of the product of a number of pairs
-        of random variables.
-
-    Notes
-    -----
-    The formula for the product of independent variables can be found at
-    https://en.wikipedia.org/wiki/Variance#Product_of_independent_variables.
-
-    """
-    assert (
-        len(mean_left) == len(mean_right) == len(var_left) == len(var_right)
-    ), "Mean and var vectors must have same length."
-
-    sub_vars = []
-
-    for i in range(len(mean_left)):
-        sub_var_temp = (
-            (mean_left[i] ** 2 * var_right[i])
-            + (mean_right[i] ** 2 * var_left[i])
-            + (var_left[i] * var_right[i])
-        )
-        sub_vars.append(sub_var_temp)
-
-    var = sum(sub_vars)
-
-    return var
-
-
 params, options = rp.get_example_model("kw_94_one", with_data=False)
 
 df = pd.read_csv("../csv/table41_columns_kw_94.csv", sep=",", index_col=0)
@@ -92,20 +44,21 @@ cov[37, 37] = -df.loc["beta2", "var"]
 # gamma
 cov[42, 42] = df.loc["gamma0", "var"]
 
-# epsilons
-eta_mean = [0, 0, 0, 0]
-eta_var = [1, 1, 1, 1]
+# as
 
-as_mean = df["true"].iloc[16:].tolist()
-as_var = df["var"].iloc[16:].tolist()
+# diagonal
+cov[47, 47] = df.loc["a11", "var"]
+cov[48, 48] = df.loc["a22", "var"]
+cov[49, 49] = df.loc["a22", "var"]
+cov[50, 50] = df.loc["a22", "var"]
 
-cov[47, 47] = var_epsilon([as_mean[0]], [eta_mean[0]], [as_var[0]], [eta_var[0]])
-
-cov[48, 48] = var_epsilon(as_mean[1:3], eta_mean[0:2], as_var[1:3], eta_var[0:2])
-
-cov[49, 49] = var_epsilon(as_mean[3:6], eta_mean[0:3], as_var[3:6], eta_var[0:3])
-
-cov[50, 50] = var_epsilon(as_mean[6:11], eta_mean[:4], as_var[6:11], eta_var[0:4])
+# non-diagonal
+cov[47, 47] = df.loc["a21", "var"]
+cov[47, 47] = df.loc["a31", "var"]
+cov[47, 47] = df.loc["a32", "var"]
+cov[47, 47] = df.loc["a41", "var"]
+cov[47, 47] = df.loc["a42", "var"]
+cov[47, 47] = df.loc["a43", "var"]
 
 cov_list = cov.tolist()
 
