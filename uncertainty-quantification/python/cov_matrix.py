3b56d3078f875ee3a562d69c8bbbb512fca3f341
tobiasstenzel <tobias.stenzel@mailbox.org>
2019-09-10 15:13:57 +0200

    hardcoded transform function with locs

--- uncertainty-quantification/python/cov_matrix.py
+++ uncertainty-quantification/python/cov_matrix.py
@@ -18,31 +18,23 @@ def create_cov_matrix():
     - The code is written simple and explicit in order to be easy to check
       and to update.
 
+    Return
+    ------
+    cov_df (DataFrame):
+        Covariance Matrix labeled by variable names on both axes
     """
     params, options = rp.get_example_model("kw_94_one", with_data=False)
 
-    df = pd.read_csv("csv/table41_kw_94.csv", sep=",", index_col=0)
+    df = pd.read_csv("csv/table41_kw_94.csv", sep=",", index_col=1)
     df["var"] = df["sd"] ** 2
 
-    cov = np.zeros((len(params), len(params)))
+    cov_df = pd.DataFrame(
+        data=np.zeros((len(df["sd"]), len(df["sd"]))),
+        index=df["parameter"].values,
+        columns=df["parameter"].values,
+    )
 
     # alphas, betas and gamma order coincides in respy and KW94.
-    np.fill_diagonal(cov[1:17, 1:17], df["var"][0:16])
+    np.fill_diagonal(cov_df.values, df["var"].values)
 
-    # as: Attentation! order in respy differs from KW94.
-
-    # diagonal
-    cov[17, 17] = df.loc["a11", "var"]
-    cov[18, 18] = df.loc["a22", "var"]
-    cov[19, 19] = df.loc["a33", "var"]
-    cov[20, 20] = df.loc["a44", "var"]
-
-    # non-diagonal
-    cov[21, 21] = df.loc["a21", "var"]
-    cov[22, 22] = df.loc["a31", "var"]
-    cov[23, 23] = df.loc["a32", "var"]
-    cov[24, 24] = df.loc["a41", "var"]
-    cov[25, 25] = df.loc["a42", "var"]
-    cov[26, 26] = df.loc["a43", "var"]
-
-    return cov
+    return cov_df
--- uncertainty-quantification/python/mc_uncertainty_propagation.py
+++ uncertainty-quantification/python/mc_uncertainty_propagation.py
@@ -1,6 +1,8 @@
 import json
 
 import numpy as np
+import pandas as pd
+import respy as rp
 from python.model_wrapper import model_wrapper_kw_94
 
 
@@ -17,10 +19,10 @@ def mc_uncertainty_propagation(mean, cov, n_draws, save_json=False):
 
     Parameters
     ----------
-    mean: array_like
-        Vector of mean of input parameters.
-    cov: array_like
-        Covariance Matrix of input parameters distribution.
+    mean: pandas.Series
+        Vector of mean of input parameters labeled by parameter names.
+    cov: DataFrame
+        Covariance Matrix labeled by variable names on both axes
     n_draws: int
         number of random draws of input parameters.
     save_json: bool
@@ -44,7 +46,7 @@ def mc_uncertainty_propagation(mean, cov, n_draws, save_json=False):
         np.random.seed(i + 100)
         kw94_params = np.random.multivariate_normal(mean, cov)
         respy_params = transform_params_kw94_respy(kw94_params)
-        qoi[i] = model_wrapper_kw_94(respy_params)
+        qoi[i] = model_wrapper_kw_94(respy_params.values)
 
     if save_json is True:
         with open("json/qoi.json", "w") as write_file:
@@ -55,40 +57,87 @@ def mc_uncertainty_propagation(mean, cov, n_draws, save_json=False):
     return qoi
 
 
-def transform_params_kw94_respy(params):
+def transform_params_kw94_respy(kw94_params):
     """
     Transforms parameters vector in format of KW94 (see table 4.1)
-    to respy format. The difference is that KW94 specifyies the distribution of
+    to respy format.
+    It accounts for the the difference between respy and KW94 in two points:
+    - The order of a-parameters differs
+    - The factors for squared experience and beta2 are multiplied by (-1) in respy
+    - KW94 specifyies the distribution of
     elements in a Cholesky Decomposition matrix but respy takes
     Standard deviations and Correlations.
 
     Parameters
     ----------
-    params: array_like
+    kw94_params: pd.Series indexed by KW94 Paramter names
         Vector of model input parameters in KW94 format.
 
     Returns
     -------
-    params: array_like
+    params: pd.Series indexed by respy Paramter names
         Vector of model input parameters in respy format.
 
     """
+    assert len(kw94_params) == 26, "Length of KW94 vector must be 26."
+
+    params, _ = rp.get_example_model("kw_94_one", with_data=False)
+
+    respy_params = pd.Series(
+        data=np.full(len(params["value"].values), np.nan), index=params.index
+    )
+
+    # Copy values that are not in KW94 from respy paramters.
+    respy_params[("delta", "delta")] = params.loc[("delta", "delta"), "value"]
+    respy_params[("meas_error", "sd_a")] = params.loc[("meas_error", "sd_a"), "value"]
+    respy_params[("meas_error", "sd_b")] = params.loc[("meas_error", "sd_b"), "value"]
+
+    # Set values that are transformed with *(-1) by respy
+    # square experiences alphas
+    respy_params[("wage_a", "exp_a_square")] = -kw94_params["alpha13"]
+    respy_params[("wage_a", "exp_b_square")] = -kw94_params["alpha15"]
+    respy_params[("wage_b", "exp_b_square")] = -kw94_params["alpha23"]
+    respy_params[("wage_b", "exp_a_square")] = -kw94_params["alpha25"]
+    # betas
+    respy_params[("nonpec_edu", "constant")] = -kw94_params["beta0"]
+    respy_params[("nonpec_edu", "at_least_twelve_exp_edu")] = -kw94_params["beta1"]
+    respy_params[("nonpec_edu", "not_edu_last_period")] = -kw94_params["beta2"]
+
+    # Set SDs and Corrs that are Cholesky elements in KW94.
     chol = np.zeros((4, 4))
-    np.fill_diagonal(chol, params[17:21])
-    chol[1, 0] = params[21]
-    chol[2, :2] = [params[22], params[23]]
-    chol[3, :3] = [params[24], params[25], params[26]]
+    np.fill_diagonal(chol, kw94_params[["a11", "a22", "a33", "a44"]])
+    chol[1, 0] = kw94_params["a21"]
+    chol[2, :2] = [kw94_params["a31"], kw94_params["a32"]]
+    chol[3, :3] = [kw94_params["a41"], kw94_params["a42"], kw94_params["a43"]]
 
     cov = np.matmul(chol, chol.T)
-
     sd = np.sqrt(np.diag(cov))
 
-    params[17:21] = sd
-    params[21] = cov[1, 0] / (sd[1] * sd[0])
-    params[22] = cov[2, 0] / (sd[2] * sd[0])
-    params[23] = cov[2, 1] / (sd[2] * sd[1])
-    params[24] = cov[3, 0] / (sd[3] * sd[0])
-    params[25] = cov[3, 1] / (sd[3] * sd[1])
-    params[26] = cov[3, 2] / (sd[3] * sd[2])
+    respy_params[("shocks", "sd_a")] = sd[0]
+    respy_params[("shocks", "sd_b")] = sd[1]
+    respy_params[("shocks", "sd_edu")] = sd[2]
+    respy_params[("shocks", "sd_home")] = sd[3]
+    respy_params[("shocks", "corr_b_a")] = cov[1, 0] / (sd[1] * sd[0])
+    respy_params[("shocks", "corr_edu_a")] = cov[2, 0] / (sd[2] * sd[0])
+    respy_params[("shocks", "corr_edu_b")] = cov[2, 1] / (sd[2] * sd[1])
+    respy_params[("shocks", "corr_home_a")] = cov[3, 0] / (sd[3] * sd[0])
+    respy_params[("shocks", "corr_home_b")] = cov[3, 1] / (sd[3] * sd[1])
+    respy_params[("shocks", "corr_home_edu")] = cov[3, 2] / (sd[3] * sd[2])
 
-    return params
+    # Fill in KW94 paramters that are not transformed.
+    # alphas
+    respy_params[("wage_a", "constant")] = kw94_params["alpha10"]
+    respy_params[("wage_a", "exp_edu")] = kw94_params["alpha11"]
+    respy_params[("wage_a", "exp_a")] = kw94_params["alpha12"]
+    respy_params[("wage_a", "exp_b")] = kw94_params["alpha14"]
+
+    # betas
+    respy_params[("wage_b", "constant")] = kw94_params["alpha20"]
+    respy_params[("wage_b", "exp_edu")] = kw94_params["alpha21"]
+    respy_params[("wage_b", "exp_a")] = kw94_params["alpha22"]
+    respy_params[("wage_b", "exp_b")] = kw94_params["alpha24"]
+
+    # gamma
+    respy_params[("nonpec_home", "constant")] = kw94_params["gamma0"]
+
+    return respy_params
